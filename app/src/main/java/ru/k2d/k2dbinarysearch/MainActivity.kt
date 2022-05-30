6b62de3d667db28fe4b59c89e41f4b2a69ebde94
Kirillov Dmitry <zxtreme@yandex.ru>
2022-05-30 20:34:41 +0300

    Change deprecated 'setOnNavigationItemSelectedListener' on 'setOnItemSelectedListener'. New try.

--- app/src/main/java/ru/k2d/k2dbinarysearch/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dbinarysearch/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         bottom_navigation.itemIconTintList = null
 
         replaceFragment(HomeFragment())
+        //test comment for test do git works
         bottom_navigation.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.ic_home_icon -> replaceFragment(HomeFragment())
