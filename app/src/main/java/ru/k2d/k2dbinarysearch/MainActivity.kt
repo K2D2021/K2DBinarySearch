98ae622b3fc1810cf9db4b55ae196f89da20e208
Kirillov Dmitry <zxtreme@yandex.ru>
2022-06-03 14:00:56 +0300

    Find nice way to work with backstack and exit if we on last fragment.

--- app/src/main/java/ru/k2d/k2dbinarysearch/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dbinarysearch/MainActivity.kt
@@ -1,17 +1,17 @@
 package ru.k2d.k2dbinarysearch
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import kotlinx.android.synthetic.main.activity_main.*
 import ru.k2d.k2dbinarysearch.fragments.GameFragment
 import ru.k2d.k2dbinarysearch.fragments.HistoryFragment
 import ru.k2d.k2dbinarysearch.fragments.HomeFragment
 import ru.k2d.k2dbinarysearch.fragments.OtherFragment
 
+
 class MainActivity : AppCompatActivity() {
-    private var doubleBackToExitPressedOnce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
                 R.id.ic_other_icon -> replaceFragment(OtherFragment())
             }
         }
-
     }
 
     fun replaceFragment(fragment: Fragment) {
@@ -43,36 +42,15 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.fragment_container, fragment)
         transaction.addToBackStack("TAG")
         transaction.commit()
-        val count = supportFragmentManager.backStackEntryCount
-            Toast.makeText(this, count.toString(), Toast.LENGTH_SHORT).show()
-
     }
-    /*override fun onBackPressed() {
-         if(BackS)
-         if (doubleBackToExitPressedOnce) {
-             super.onBackPressed()
-             return
-         }
-
-         this.doubleBackToExitPressedOnce = true
-         Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
-
-         //Handler(Looper.getMainLooper()).postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
-     }*/
 
     override fun onBackPressed() {
-        val count = supportFragmentManager.backStackEntryCount
-        Toast.makeText(this, count.toString(), Toast.LENGTH_SHORT).show()
-        if (count == 2) {
-            if (doubleBackToExitPressedOnce) {
-                super.onBackPressed()
-                return
-            }
+        val manager: FragmentManager = supportFragmentManager
+        if (manager.backStackEntryCount > 1) {
+            manager.popBackStack()
         } else {
-            //Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
-            super.onBackPressed()
+            moveTaskToBack(true)
         }
-        this.doubleBackToExitPressedOnce = true
     }
 }
 
