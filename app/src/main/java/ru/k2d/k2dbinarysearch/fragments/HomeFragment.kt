e588952930f2e79bde5fdff86508beb5c8a098e6
K2D2021 <zxtreme@yandex.ru>
2022-07-28 17:19:54 +0300

    Deleted garbage code.

--- app/src/main/java/ru/k2d/k2dbinarysearch/fragments/HomeFragment.kt
+++ app/src/main/java/ru/k2d/k2dbinarysearch/fragments/HomeFragment.kt
@@ -9,20 +9,7 @@ import kotlinx.android.synthetic.main.fragment_home.*
 import ru.k2d.k2dbinarysearch.MainActivity
 import ru.k2d.k2dbinarysearch.R
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class HomeFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
--- app/src/main/java/ru/k2d/k2dbinarysearch/fragments/OtherFragment.kt
+++ app/src/main/java/ru/k2d/k2dbinarysearch/fragments/OtherFragment.kt
@@ -11,27 +11,9 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import ru.k2d.k2dbinarysearch.R
 
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class OtherFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
     private lateinit var imageV: ImageView
     private lateinit var other_fragment: FrameLayout
-
-    //lateinit var bitmap: Bitmap
-//    imageV = findViewById(R.id.imageV)
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,7 +31,6 @@ class OtherFragment : Fragment() {
         AnimationDrawable.setEnterFadeDuration(1500)
         AnimationDrawable.setExitFadeDuration(3000)
         AnimationDrawable.start()
-        //imageV.background = BitmapDrawable(getResources(), bitmap)*/
         super.onViewCreated(view, savedInstanceState)
     }
 
