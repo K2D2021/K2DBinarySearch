79c3921ff4629cff0885caf42766bc5958953b83
K2D2021 <zxtreme@yandex.ru>
2022-08-16 23:15:57 +0300

    Update dependencies.

--- app/src/main/java/ru/k2d/k2dbinarysearch/data/AppDatabase.kt
+++ app/src/main/java/ru/k2d/k2dbinarysearch/data/AppDatabase.kt
@@ -5,9 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import ru.k2d.k2dbinarysearch.DBHistoryItem
-
 @Database(
-    entities = [DBHistoryItem::class], version = 1
+    entities = [DBHistoryItem::class], version = 1, exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun historyItemDAO(): HistoryItemDAO
